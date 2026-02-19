2c0c07268292313f490714b99fedb36262e8f233
quattro <nick.mancuso@gmail.com>
2026-02-18 16:43:50 -0800

    feat: extract pure curve fit numerics with Solution contract

--- src/mut_var/numerics/__init__.py
+++ src/mut_var/numerics/__init__.py
@@ -1,4 +1,5 @@
 from .baseline import BaselineConfig, Params, fit_baseline
+from .curve_fit import curve, fit_curve
 from .pipeline import InferenceArrays, InferenceConfig, run_inference_pipeline
 from .refit import RefitConfig, fit_refit_grid
 
@@ -8,7 +9,9 @@ __all__ = [
     "InferenceConfig",
     "Params",
     "RefitConfig",
+    "curve",
     "fit_baseline",
+    "fit_curve",
     "fit_refit_grid",
     "run_inference_pipeline",
 ]
